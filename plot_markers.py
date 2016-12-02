b3eae83569eb272980e3b93dd47db20a3b436fbd
Øyvind Helgeland <helgeland.oyvind@gmail.com>
2016-12-02 15:18:24 +0100

    Fixed some bugs in the code - added removal of temporary files after script finished

--- plot_markers.py
+++ plot_markers.py
@@ -15,7 +15,7 @@ parser.add_argument('-o', '--outpath', help='output path of plots and index')
 parser.add_argument('-r', '--reindex', help='reindex intensity file', action='store_true')
 args = parser.parse_args()
 
-
+# Open full table file
 table = open(args.table, "r")
 
 
@@ -27,14 +27,9 @@ def index_intensity_file(f):
     pos = f.tell()
 
     # Read the first line, cursor will move to beginning of next line
+    # ie. it will skip the first line with the sample headers when
+    # creating the indexfile
     line= f.readline()
-
-    # Get the name of the "marker" in the first line, will be "Name"
-    marker = line.partition("\t")[0]
-
-    # Create the row and append to list
-    tmp = marker + "\t" + str(pos)
-    index.append(tmp)
 
     # Loop over rest of file
     while line:
@@ -64,7 +59,7 @@ if args.reindex:
 	index_intensity_file(table)
 	ind = os.path.join(args.outpath, args.indexfile)
 else:
-	ind=args.indexfile
+	ind= os.path.join(args.outpath, args.indexfile)
 
 # Load indexfile into lookup dictionary
 lookup = {}
@@ -97,11 +92,12 @@ attach_header()
 
 for m in open(args.markerlist, "r"):
 	marker = m.strip()
-	fetch_intensity_row(marker, lookup[marker])
+	# Check if marker in list exists in lookuptable (aka. indexfile)
+	if marker in lookup:
+	    fetch_intensity_row(marker, lookup[marker])
 
-#print(rowlist)
 
-# Write output
+# Write only the rows to tmp output before sending it to R script
 tmpout = open(os.path.join(args.outpath, 'tmpout.txt'), 'a+')
 for i in rowlist:
    tmpout.write(i)
@@ -111,9 +107,5 @@ tmpout.close()
 # Call Rscript for plotting the markers
 subprocess.call (["/usr/bin/Rscript", "lib/make_cluster_plot.R", os.path.join(args.outpath, 'tmpout.txt'), args.outpath])
 
-
-# Specify raw intensity file
-#file = "/media/local-disk/common/gsexport/moba12-1692-bad-plate-samples-reclust/moba12-1692-bad-plate-samples-fulldata.txt"
-
-# Open the file with read only permission
-#f = open(file, "r")
+# Remove tmpout.txt
+os.remove(os.path.join(args.outpath, 'tmpout.txt'))
